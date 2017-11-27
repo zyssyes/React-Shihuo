37cbc8b913005fc3b75b35992b6af6b7939ae066
zhangjing789987 <1753213666@qq.com>
2017-11-27 14:53:47 +0800

    add loop

--- shihuo/src/Detail.js
+++ shihuo/src/Detail.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import { Carousel } from 'antd';
+import 'antd/dist/antd.css'
 import './Detail.css';
+import Icon from 'antd/lib/icon';
 
 class Detail extends Component {
   render() {
     return (
       <div className="Detail">
-       
+        <div className='loop'>
+                <Carousel autoplay>
+                  <div><img src='../img/loop1.jpg' alt='图片' /></div>
+                  <div><img src='../img/loop2.jpg' alt='图片' /></div>
+                  <div><img src='../img/loop3.jpg' alt='图片' /></div>
+                  <div><img src='../img/loop4.jpg' alt='图片' /></div>
+                  <div><img src='../img/loop5.jpg' alt='图片' /></div>
+                </Carousel>
+        </div>
+        <div className='icon'>
+          <div><Icon type="team" /><span></span></div>
+          <div><Icon type="schedule" /></div>
+          <div><Icon type="global" /></div>
+          <div><Icon type="shake" /></div>
+        </div>
       </div>
     );
   }
