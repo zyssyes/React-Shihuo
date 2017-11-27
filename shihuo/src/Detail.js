a87292bba51c54210fd8bfd00e5a3a76f7aae038
zhangjing789987 <1753213666@qq.com>
2017-11-27 18:58:38 +0800

    add before all

--- shihuo/src/Detail.js
+++ shihuo/src/Detail.js
@@ -18,10 +18,29 @@ class Detail extends Component {
                 </Carousel>
         </div>
         <div className='icon'>
-          <div><Icon type="team" /><span></span></div>
-          <div><Icon type="schedule" /></div>
-          <div><Icon type="global" /></div>
-          <div><Icon type="shake" /></div>
+          <div className='list'><Icon type="team" className='sml_icon'><span>众测</span></Icon></div>
+          <div className='list'><Icon type="schedule" className='sml_icon'><span>栏目</span></Icon></div>
+          <div className='list'><Icon type="global" className='sml_icon'><span>最新</span></Icon></div>
+          <div className='list'><Icon type="shake" className='sml_icon'><span>最热</span></Icon></div>
+        </div>
+        <div className='title'>
+          <strong><p>热门话题</p></strong>
+          <div className='title_loop'>
+            <div className='title_div'><img src='../img/title1.jpg' alt='图片' /><div className='opt'><p># 跑步鞋会丨晒晒你的装备美图 #</p></div></div>
+            <div className='title_div'><img src='../img/title2.jpg' alt='图片' /><div className='opt'><p># 你型，你来晒！晒发型，送发油活动 #</p></div></div>
+            <div className='title_div'><img src='../img/title3.jpg' alt='图片' /><div className='opt'><p># 爱拍也爱练 | 见证你的改变 #</p></div></div>
+          </div>
+        </div>
+        <div className='all'>
+          <ul>
+            <li><a>全部</a></li>
+            <li><a>实战推荐</a></li>
+            <li><a>潮流穿搭</a></li>
+            <li><a>球鞋90秒</a></li>
+            <li><a>跑步有道</a></li>
+            <li><a>测评档案</a></li>
+            <li><a>数码</a></li>
+          </ul>
         </div>
       </div>
     );
