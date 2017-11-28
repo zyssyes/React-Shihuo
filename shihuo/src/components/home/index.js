dd72762b60bebbbfcf993e0758cccfd3c5738594
zyssyes <zyssyes@163.com>
2017-11-28 17:01:02 +0800

    6

--- shihuo/src/components/home/index.js
+++ shihuo/src/components/home/index.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react';
 import "./index.css";
 import { Tabs, Radio } from 'antd';
 import { Carousel } from 'antd';
-
+import axios from 'axios'
 	
 const TabPane = Tabs.TabPane;
 class Home extends React.Component {
@@ -10,7 +10,18 @@ class Home extends React.Component {
     super(props);
     this.state = {
       mode: 'top',
+      showList:[]
     };
+  }
+  componentDidMount(){
+  	var that = this;
+  	axios.get("/haitao/list?r=0&page=1&page_size=20")
+  		.then((res)=>{
+  			console.log(res)
+  			that.setState({
+  				showList:res.data.data
+  			})
+  		})
   }
   handleModeChange = (e) => {
     const mode = e.target.value;
@@ -38,9 +49,27 @@ class Home extends React.Component {
                 </Carousel>
                 <img id='img1'src="../img/6.jpg" alt="" />
                 <img id='img2' src="../img/7.jpg " alt=""/>
+                <div className='show'>
+                	{
+                		this.state.showList.map((item,index)=>{
+                			return(
+                				<div key={item.id}>
+                					<img src={item.img} />
+                					<p>{item.title}</p>
+                					<span>${item.price}</span><span>{item.business}</span>
+                				</div>
+                				)
+                		})
+                	}
+                </div>
         	</div>
           </TabPane>
-          <TabPane tab="户外运动" key="2">111</TabPane>
+          <TabPane tab="户外运动" key="2">
+          	<ul>
+          		
+          	<ul>
+
+          </TabPane>
           <TabPane tab="休闲鞋服" key="3">11</TabPane>
           <TabPane tab="饰品手表" key="4">11</TabPane>
           <TabPane tab="饰品营销" key="5">11</TabPane>
